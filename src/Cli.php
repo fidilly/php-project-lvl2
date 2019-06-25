1665fd903b134ee5b9302e8c20e7fbc2828e4a60
fidilly <fidship@hotmail.com>
2019-06-25 13:50:31 +0400

    fix version number

--- src/Cli.php
+++ src/Cli.php
@@ -18,7 +18,7 @@ function run()
         -v --version      Show version.
     DOC;
     
-    $args = Docopt::handle($doc, ['version' => 'gendiff 0.0.0']);
+    $args = Docopt::handle($doc, ['version' => 'gendiff 0.0.1']);
     foreach ($args as $k => $v) {
         echo $k.': '.json_encode($v).PHP_EOL;
     }
