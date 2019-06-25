1b2748db76d36ebcb9cc4888ee4952e0495889c7
fidilly <fidship@hotmail.com>
2019-06-25 16:24:02 +0400

    Updating files

--- src/Cli.php
+++ src/Cli.php
@@ -6,17 +6,17 @@ use Docopt;
 function run()
 {
     $doc = <<<DOC
-    Generate diff
+Generate diff
 
-    Usage:
-        gendiff (-h|--help)
-        gendiff [--format <fmt>] <firstFile> <secondFile>
+Usage:
+    gendiff (-h|--help)
+    gendiff [--format <fmt>] <firstFile> <secondFile>
 
-    Options:
-        -h --help         Show this screen.
-        --format <fmt>    Report format [default: pretty].
-        -v --version      Show version.
-    DOC;
+Options:
+    -h --help         Show this screen.
+    --format <fmt>    Report format [default: pretty].
+    -v --version      Show version.
+DOC;
     
     $args = Docopt::handle($doc, ['version' => 'gendiff 0.0.1']);
     foreach ($args as $k => $v) {
