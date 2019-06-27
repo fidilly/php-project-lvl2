b5a9f9a3b8a90255e672e9e0f4cca4eb478d57b3
fidilly <fidship@hotmail.com>
2019-06-27 17:59:01 +0400

    Added new functionality

--- src/genDiff.php
+++ src/genDiff.php
@@ -2,6 +2,7 @@
 
 namespace Differ;
 use Docopt;
+use function Funct\Collection\union;
 
 function run()
 {
@@ -21,13 +22,42 @@ DOC;
     $args = Docopt::handle($doc, ['version' => 'gendiff 0.0.1']);
     $pathToFile1 = './' . $args['<firstFile>'];
     $pathToFile2 = './' . $args['<secondFile>'];
-    genDiff($pathToFile1, $pathToFile2);
+    echo genDiff($pathToFile1, $pathToFile2);
 }
 
 function gendiff($pathToFile1, $pathToFile2)
 {
     if (file_exists($pathToFile1) && file_exists($pathToFile2)) {
-        $File1Content = json_decode(file_get_contents($pathToFile1));
-        $File2Content = json_decode(file_get_contents($pathToFile2));
+        $file1Content = json_decode(file_get_contents($pathToFile1), true);
+        $file2Content = json_decode(file_get_contents($pathToFile2), true);
+    }
+    
+    $combCont = union($file1Content, $file2Content);
+    $diffCont = array_diff_key($file1Content, $file2Content);
+    $differ = array_reduce(array_keys($combCont), function ($acc, $key) use ($combCont, $diffCont, $file1Content) {
+        if (array_key_exists($key, $file1Content) && !array_key_exists($key, $diffCont)) {
+            $combContValue = boolToString($combCont[$key]);
+            $file1Value = boolToString($file1Content[$key]);
+            if ($combContValue === $file1Value) {
+                return $acc . "    $key: $combContValue\n";
+            } else {
+                return $acc . "  + $key: $combContValue\n  - $key: $file1Value\n";
+            }
+        } elseif (array_key_exists($key, $diffCont)) {
+            $diffContValue = boolToString($diffCont[$key]);
+            return $acc . "  - $key: $diffContValue\n";
+        } else {
+            $combContValue = boolToString($combCont[$key]);
+            return $acc . "  + $key: $combContValue\n";
+        }
+    }, "{\n") . "}\n";
+    return $differ;
+}
+
+function boolToString ($value) {
+    if (is_bool($value)) {
+        return $value ? 'true' : 'false';
+    } else {
+        return $value;
     }
 }
