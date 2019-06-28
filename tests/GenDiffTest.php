725665d25cf0bd578e5373d3994a304df3d5b862
fidilly <fidship@hotmail.com>
2019-06-28 14:50:11 +0400

    Add tests

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 
 class GenDiffTest extends TestCase
 {
-    public function testGenDiff()
+    public function testGenDiffJson()
     {
         $expected = <<<EOD
 {
@@ -24,6 +24,24 @@ EOD;
         $actual = genDiff(__DIR__ . "/before.json", __DIR__ . "/after.json");
         $this->assertEquals($expected, $actual);
     }
+
+    public function testGenDiffYaml()
+    {
+        $expected = <<<EOD
+{
+    host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  + verbose: true
+}
+
+EOD;
+
+        $actual = genDiff(__DIR__ . "/before.yaml", __DIR__ . "/after.yaml");
+        $this->assertEquals($expected, $actual);
+    }
+
 
     public function testBoolToString()
     {
