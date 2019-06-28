c5a28bae43618cfc3ffff5b83d99542e451a194a
fidilly <fidship@hotmail.com>
2019-06-28 14:51:53 +0400

    Added yaml file comparison functionality

--- src/genDiff.php
+++ src/genDiff.php
@@ -3,6 +3,8 @@
 namespace Differ;
 use Docopt;
 use function Funct\Collection\union;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Exception\ParseException;
 
 function run()
 {
@@ -27,9 +29,11 @@ DOC;
 
 function gendiff($pathToFile1, $pathToFile2)
 {
-    if (file_exists($pathToFile1) && file_exists($pathToFile2)) {
-        $file1Content = json_decode(file_get_contents($pathToFile1), true);
-        $file2Content = json_decode(file_get_contents($pathToFile2), true);
+    $content = getContents($pathToFile1, $pathToFile2);
+    if (!is_null($content)) {
+        [$file1Content, $file2Content] = $content;
+    } else {
+        return;
     }
     
     $combCont = union($file1Content, $file2Content);
@@ -60,5 +64,27 @@ function boolToString($value)
         return $value ? 'true' : 'false';
     } else {
         return $value;
+    }
+}
+
+function getContents($pathToFile1, $pathToFile2)
+{
+    if (file_exists($pathToFile1) && file_exists($pathToFile2)) {
+        $jsonContent1 = json_decode(file_get_contents($pathToFile1), true);
+        if (json_last_error() === 0) {
+            $jsonContent2 = json_decode(file_get_contents($pathToFile2), true);
+            if (json_last_error() === 0) {
+                $file1Content = $jsonContent1;
+                $file2Content = $jsonContent2;
+                return [$file1Content, $file2Content];
+            }
+        } else {
+            try {
+                $file1Content = Yaml::parseFile($pathToFile1);
+                $file2Content = Yaml::parseFile($pathToFile2);
+                return [$file1Content, $file2Content];
+            } catch (ParseException $exception) {
+            }
+        }
     }
 }
