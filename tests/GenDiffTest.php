c09f1f03f1b29c2733c34bd81f5fd72e2088e900
fidilly <fidship@hotmail.com>
2019-06-28 21:07:52 +0400

    Add tests

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -21,8 +21,22 @@ class GenDiffTest extends TestCase
 
 EOD;
 
-        $actual = genDiff(__DIR__ . "/before.json", __DIR__ . "/after.json");
-        $this->assertEquals($expected, $actual);
+        $actual1 = genDiff(__DIR__ . "/before.json", __DIR__ . "/after.json");
+        $this->assertEquals($expected, $actual1);
+        
+        $expected = <<<EOD
+{
+    host: hexlet.io
+  + timeout: 20
+  - timeout: 50
+  - proxy: 123.234.53.22
+  + verbose: true
+}
+
+EOD;
+
+        $actual2 = genDiff(__DIR__ . "/nestedBefore.json", __DIR__ . "/nestedAfter.json");
+        $this->assertEquals($expected, $actual2);
     }
 
     public function testGenDiffYaml()
