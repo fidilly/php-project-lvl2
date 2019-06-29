85fce4f98f12f8a46e8ea3159b3d554c3214999c
fidilly <fidship@hotmail.com>
2019-06-29 18:59:48 +0400

    Update displayAst

--- src/displayDiffAst.php
+++ src/displayDiffAst.php
@@ -9,21 +9,28 @@ function displayDiffAst($ast)
     $rendering = array_reduce($ast, function ($acc, $item) {
         $data = $item['data'];
         $key = $item['key'];
-        $before = $item['before'];
-        $after = $item['after'];
-        $afterText = "$key: $after";
-        $beforeText = "$key: $before";
+        $before = boolToString($item['before']);
+        $after = boolToString($item['after']);
         
         if ($data === 'unchanged') {
-            return $acc . "    $afterText\n";
+            return $acc . "    $key: $after\n";
         } elseif ($data === 'changed') {
-            return $acc . "  + $afterText\n" . "  - $beforeText\n";
+            return $acc . "  + $key: $after\n" . "  - $key: $before\n";
         } elseif ($data === 'added') {
-            return $acc . "  + $afterText\n";
+            return $acc . "  + $key: $after\n";
         } elseif ($data === 'removed') {
-            return $acc . "  - $beforeText\n";
+            return $acc . "  - $key: $before\n";
         }
     }, "{\n") . "}\n";
 
     return $rendering;
+}
+
+function boolToString($value)
+{
+    if (is_bool($value)) {
+        return $value ? 'true' : 'false';
+    } else {
+        return $value;
+    }
 }
