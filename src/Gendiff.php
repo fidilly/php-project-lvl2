a04a2172d6fa3901c04d7bc2d9901bee9e71b529
fidilly <fidship@hotmail.com>
2019-07-14 21:16:51 +0400

    Update gendiff file

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -9,44 +9,37 @@ use Symfony\Component\Yaml\Exception\ParseException;
 
 function gendiff($pathToFile1, $pathToFile2, $format)
 {
-    $content = getContents($pathToFile1, $pathToFile2);
-    if (!is_null($content)) {
-        [$file1Content, $file2Content] = $content;
+    $file1Content = getContent($pathToFile1);
+    $file2Content = getContent($pathToFile2);
+    if (!is_null($file1Content) && !is_null($file2Content)) {
+        $ast = makeDiffAst($file1Content, $file2Content);
     } else {
         return;
     }
-
-    $ast = makeDiffAst($file1Content, $file2Content);
     return displayDiff($ast, $format);
 }
 
-function getContents($pathToFile1, $pathToFile2)
+function getContent($pathToFile)
 {
-    if (isJsonFiles($pathToFile1, $pathToFile2)) {
-        $file1Content = json_decode(file_get_contents($pathToFile1), true);
-        $file2Content = json_decode(file_get_contents($pathToFile2), true);
-        return [$file1Content, $file2Content];
-    } else {
-        try {
-            $file1Content = Yaml::parseFile($pathToFile1);
-            $file2Content = Yaml::parseFile($pathToFile2);
-            return [$file1Content, $file2Content];
-        } catch (ParseException $exception) {
-            echo 'Invalid file path';
+    if (file_exists($pathToFile)) {
+        if (isJsonFile($pathToFile)) {
+            $fileContent = json_decode(file_get_contents($pathToFile), true);
+            return $fileContent;
+        } else {
+            try {
+                $fileContent = Yaml::parseFile($pathToFile);
+                return $fileContent;
+            } catch (ParseException $exception) {
+            }
         }
     }
 }
 
-function isJsonFiles($pathToFile1, $pathToFile2)
+function isJsonFile($pathToFile)
 {
-    if (file_exists($pathToFile1) && file_exists($pathToFile2)) {
-        $file1Content = json_decode(file_get_contents($pathToFile1));
-        if (json_last_error() === 0) {
-            $file2Content = json_decode(file_get_contents($pathToFile2));
-            if (json_last_error() === 0) {
-                return true;
-            }
-        }
+    $fileContent = json_decode(file_get_contents($pathToFile));
+    if (json_last_error() === 0) {
+            return true;
     }
     return false;
 }
